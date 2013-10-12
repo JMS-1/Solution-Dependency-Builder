3d7f239ea9f31243273c91216e39f1aa4cb5125f
JMS-1 <dev-2@psimarron.net>
2013-10-12 12:57:22 +0200

    Initial upload - no functionality in it!

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace JMS.Tools.SolutionUpdater
+{
+    /// <summary>
+    /// Repräsentiert den Windows Prozess als Ganzes.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Gesetzt, wenn die Protokollierung aktiviert ist.
+        /// </summary>
+        private static bool _Logging;
+
+        /// <summary>
+        /// Startet die Anwendung.
+        /// </summary>
+        /// <param name="args">Die Liste der Befehlszeilenparameter.</param>
+        public static void Main( string[] args )
+        {
+            // Create command map
+            var map = new HashSet<string>( args, StringComparer.InvariantCultureIgnoreCase );
+
+            // Check flags
+            _Logging = map.Contains( "/log" );
+
+            // Process files
+            foreach (var path in map.Where( arg => arg.StartsWith( "/solution=", StringComparison.InvariantCultureIgnoreCase ) ).Select( arg => arg.Substring( 10 ) ))
+                Process( path );
+        }
+
+        /// <summary>
+        /// Bearbeitet eine einzelne Solution.
+        /// </summary>
+        /// <param name="path">Der volle Pfad zur Solution.</param>
+        private static void Process( string path )
+        {
+            Console.WriteLine( path );
+        }
+    }
+}
